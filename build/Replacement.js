40ded4f24917957ad56012cb0f79f8e9ed6bea26
Thomas Mortensson <thomasmortensson@gmail.com>
2015-07-27 15:30:28 +0100

    changed replacement

--- build/Replacement.js
+++ build/Replacement.js
@@ -1,38 +1,40 @@
-walk(document.body);
+// walk(document.body);
 
-function walk(node) 
-{
-	// I stole this function from here:
-	// http://is.gd/mwZp7E
+// function walk(node) 
+// {
+// 	// I stole this function from here:
+// 	// http://is.gd/mwZp7E
 	
-	var child, next;
+// 	var child, next;
 
-	switch ( node.nodeType )  
-	{
-		case 1:  // Element
-		case 9:  // Document
-		case 11: // Document fragment
-			child = node.firstChild;
-			while ( child ) 
-			{
-				next = child.nextSibling;
-				walk(child);
-				child = next;
-			}
-			break;
+// 	switch ( node.nodeType )  
+// 	{
+// 		case 1:  // Element
+// 		case 9:  // Document
+// 		case 11: // Document fragment
+// 			child = node.firstChild;
+// 			while ( child ) 
+// 			{
+// 				next = child.nextSibling;
+// 				walk(child);
+// 				child = next;
+// 			}
+// 			break;
 
-		case 3: // Text node
-			handleText(node);
-			break;
-	}
-}
+// 		case 3: // Text node
+// 			handleText(node);
+// 			break;
+// 	}
+// }
 
-function handleText(textNode) 
-{
-	var v = textNode.nodeValue;
+// function handleText(textNode) 
+// {
+// 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bGame of Thrones\b/g, "");
+// 	v = v.replace(/\bGame of Thrones\b/g, "");
 	
 	
-	textNode.nodeValue = v;
-}
+// 	textNode.nodeValue = v;
+// }
+
+document.body.innerHTML = document.body.innerHTML.replace(/Game of Thrones/g, 'Game of Bones'); 
