e830fd87e6646add0db5282e95d9a8cf7eff3c4a
Thomas Mortensson <thomasmortensson@gmail.com>
2015-07-30 20:32:50 +0100

    Fixed assumption that visibility should be initialised to true

--- build/replacement.js
+++ build/replacement.js
@@ -1,8 +1,5 @@
 $.expr[':'].containsInsensitive = $.expr.createPseudo(function(matchingText) {
 	return function(element) {
-		if($(element).text().toLowerCase().replace(' ', '').search(matchingText.replace(' ', '')) !== -1) {
-			console.log($(element).text() + " - " + matchingText);
-		}
 		return $(element).text().toLowerCase().replace(' ', '').search(matchingText.replace(' ', '')) !== -1
 	}
 });
@@ -12,38 +9,43 @@ function hideElement(element) {
 }
 
 function hideElementsContainingText(matchingText) {
+	console.log(matchingText);
 	$("*:containsInsensitive(" + matchingText + ")").each(function() {
 		hideElement($(this))
 	});
 }
 
-function hideElements(programmeList) {
-	console.log(programmeList);
-	for (show in programmeList) {
-		if (localStorage.getItem(show) != null) {
-			var checked = (localStorage.getItem(show) == "0") ? false : true;
-			if (checked) {
-				var show = programmeList[show]
-				console.log(show)
-				for (keyword_id in show) {
-					var keyword = show[keyword_id];
-					// Add in remove on below keyword
-					console.log(keyword);
-					hideElementsContainingText(keyword)
-				}
-			}
-		}
-	}
-}
+// function hideElements(programmeList) {
+// 	console.log(programmeList);
+// 	for (show in programmeList) {
+// 		if (localStorage.getItem(show) != null) {
+// 			var checked = (localStorage.getItem(show) == "0") ? false : true;
+// 			if (checked) {
+// 				// console.log(programmeList);
+// 				var show = programmeList[show]
+// 				// console.log(show)
+
+// 				for (keyword_id in show) {
+// 					var keyword = show[keyword_id];
+// 					// Add in remove on below keyword
+// 					console.log(keyword);
+// 					hideElementsContainingText(keyword)
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
 function hideSelectedShows() {
     get('SpoilerAlert', function(sp) {
         var programmeList = sp.SpoilerAlert;
+        console.log(sp);
         for (var _showName in programmeList) (function(showName) {
             if (programmeList[showName].visible) {
                 var keywords = programmeList[showName].keywords;
-                for (var keyword in keywords) {
-                    hideElementsContainingText(keyword);
+                // console.log(keywords);
+                for (var i=0; i<keywords.length; i++) {
+                    hideElementsContainingText(keywords[i]);
                 }
             }
         })(_showName);
