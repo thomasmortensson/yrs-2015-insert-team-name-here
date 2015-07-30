439b1f18a9171c22f38cb107c3f4a67dd478419d
Thomas Mortensson <thomasmortensson@gmail.com>
2015-07-30 11:22:49 +0100

    List of keywords to remove

--- build/Replacement.js
+++ build/Replacement.js
@@ -105,11 +105,15 @@
 			}, 3));
 
 function hideElements() {
-	console.log("In hide elements");
+	//console.log("In hide elements");
 	for(element in programmeList) {
-		var checked = (localStorage.getItem(element) == "0") ? false : true;
-		if(checked) {
-    		console.log(element);		
+		if(localStorage.getItem(element) != null) { 
+			var checked = (localStorage.getItem(element) == "0") ? false : true;
+			if(checked) {
+	    		for(key_id in programmeList[element]) {
+	    			console.log(programmeList[element][key_id]);
+	    		}	
+	    	}
     	}
 	}
 
