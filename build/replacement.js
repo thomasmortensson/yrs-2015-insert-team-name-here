dc3a5a54ac6c192d9af9f45513e484893aba1dd2
Thomas Mortensson <thomasmortensson@gmail.com>
2015-07-30 20:53:50 +0100

    Removed redundant localstorage code

--- build/replacement.js
+++ build/replacement.js
@@ -1,5 +1,8 @@
 $.expr[':'].containsInsensitive = $.expr.createPseudo(function(matchingText) {
 	return function(element) {
+		if($(element).text().toLowerCase().replace(' ', '').search(matchingText.replace(' ', '')) !== -1) {
+			console.log($(element).text());
+		}
 		return $(element).text().toLowerCase().replace(' ', '').search(matchingText.replace(' ', '')) !== -1
 	}
 });
@@ -9,37 +12,15 @@ function hideElement(element) {
 }
 
 function hideElementsContainingText(matchingText) {
-	console.log(matchingText);
+	// console.log(matchingText);
 	$("*:containsInsensitive(" + matchingText + ")").each(function() {
 		hideElement($(this))
 	});
 }
 
-// function hideElements(programmeList) {
-// 	console.log(programmeList);
-// 	for (show in programmeList) {
-// 		if (localStorage.getItem(show) != null) {
-// 			var checked = (localStorage.getItem(show) == "0") ? false : true;
-// 			if (checked) {
-// 				// console.log(programmeList);
-// 				var show = programmeList[show]
-// 				// console.log(show)
-
-// 				for (keyword_id in show) {
-// 					var keyword = show[keyword_id];
-// 					// Add in remove on below keyword
-// 					console.log(keyword);
-// 					hideElementsContainingText(keyword)
-// 				}
-// 			}
-// 		}
-// 	}
-// }
-
 function hideSelectedShows() {
     get('SpoilerAlert', function(sp) {
         var programmeList = sp.SpoilerAlert;
-        console.log(sp);
         for (var _showName in programmeList) (function(showName) {
             if (programmeList[showName].visible) {
                 var keywords = programmeList[showName].keywords;
